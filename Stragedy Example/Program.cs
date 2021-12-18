ed8c5daa3076ccacd609e358c9079638f5bb44d9
d30101999 <58314095+d30101999@users.noreply.github.com>
2021-12-19 00:46:20 +0300

    Add files via upload

---
+++ Stragedy Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Stragedy_Example.Promote;
+namespace Stragedy_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start getting ticket!!!!!");
+            var random = new Random();
+            for (var i = 1; i < 6; i++)
+            {
+                var stragedyIndex = random.Next(0, 3);
+                var ticket = new Ticket();
+                ticket.Name = "Ve so: " + (i);
+                ticket.Price = 100;
+                switch( stragedyIndex)
+                {
+                    case 0:
+                        ticket.PromoteStragedy = new NoDiscount();
+                        break;
+                    case 1:
+                        ticket.PromoteStragedy = new HalfDiscount();
+                        break;
+                    case 2:
+                        ticket.PromoteStragedy = new QuarterDiscount();
+                        break;
+                }
+                var promotedprice = ticket.PromotedPrice();
+                Console.WriteLine("Prompted price of " + ticket.Name + " is " + promotedprice + " " + ticket.PromoteStragedy.GetType().Name);
+
+
+                
+            }
+        }
+    }
+}
---
+++ Stragedy Example/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stragedy_Example.Promote;
+namespace Stragedy_Example
+{
+    public class Ticket
+    {
+        private IPromoteStragedy promoteStragedy;
+        private double price;
+        private string name;
+        public IPromoteStragedy PromoteStragedy { get => promoteStragedy; set => promoteStragedy = value; }
+        public double Price { get => price; set => price = value; }
+        public string Name { get => name; set => name = value; }
+
+        public Ticket()
+        { }
+        public Ticket(IPromoteStragedy promoteStragedy)
+        {
+            PromoteStragedy = promoteStragedy;
+        }
+        public double PromotedPrice()
+        {
+            return PromoteStragedy.DoDiscount(Price);
+        }
+    }
+}
