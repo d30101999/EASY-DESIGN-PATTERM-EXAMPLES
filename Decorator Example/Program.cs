3c34c097ed0b7b3388028b115b0c2ccda5f122ef
d30101999 <58314095+d30101999@users.noreply.github.com>
2021-12-19 00:41:32 +0300

    Add files via upload

---
+++ Decorator Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Decorator_Example.DecoratorMT;
+using Decorator_Example.MilkTeas;
+namespace Decorator_Example
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let make some Milk Tea!!");
+            var firstMilkTea = new EggPudding(
+                                    new FruitPudding(
+                                        new BlackSurgar(
+                                            new Bubble(
+                                                new MilkTea()))));
+            ////Cost of EggPuddingFruitPuddingBlackSurgarBubbleMilkTea 10 + 3 + 4 + 1 + 2 = 20 
+            Console.WriteLine(firstMilkTea.Cost());
+        }
+    }
+}
